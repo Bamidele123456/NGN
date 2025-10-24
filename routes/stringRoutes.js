1e3448a9e1b973822e72c1bf91a9d26f66ecd46d
Bamidele123456 <bamideleprecious85@gmail.com>
2025-10-24 22:07:16 +0100

    Added strings

--- routes/stringRoutes.js
+++ routes/stringRoutes.js
@@ -9,11 +9,12 @@ const router = express.Router();
 
 router.post("/", createString);
 router.get("/", getAllStrings);
-router.get("/:string_value", getString);
-router.delete("/:string_value", deleteString);
-router.delete("/clear", (req, res) => {
+router.post("/clear", (req, res) => {
   stringsDB.clear();
   res.status(200).json({ message: "Database cleared for now" });
 });
+router.get("/:string_value", getString);
+router.delete("/:string_value", deleteString);
+
 
 module.exports = router;
